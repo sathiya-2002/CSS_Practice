d574602fd42924fe5ceabcfea22ceb2f3c29937f
sathiya-2002 <sathiyabhamag@gmail.com>
2024-10-03 12:31:05 +0530

    updated

--- Tags/JAVA SCRIPT/Module1.js
+++
@@ -1,6 +0,0 @@
-// module1.js
-export const myFunction = () => {
-    console.log("This is a function from module1.js");
-  };
-  
-  export const myVariable = 42;
--- Tags/JAVA SCRIPT/Module2.js
+++
@@ -1,6 +0,0 @@
-// module2.js
-import { myFunction, myVariable } from "./Module1.js";
-
-myFunction();
-console.log(`myVariable: ${myVariable}`);
-
